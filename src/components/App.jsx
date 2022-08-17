31e23aaedaf81d77a6252a1433ed219bd4dfb88d
ArtemStepanov1 <sparta.start@gmail.com>
2022-08-17 13:18:53 +0300

    contacts saved in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import { ContactsList } from "./ContactsList";
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: ''
@@ -43,7 +38,19 @@ export class App extends Component {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId)
     }))
+  }
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevState) {
+    if(this.state.contacts !== prevState.contacts){
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
   }
 
   render() {
